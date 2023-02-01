87e5bdfae2b3847ad9ded839666068b4c52b0388
micka <mickavende.pro@gmail.com>
2023-02-01 16:28:55 +0100

    display post with twig

--- src/Entity/Post/Post.php
+++ src/Entity/Post/Post.php
@@ -47,7 +47,7 @@ class Post
     private ?\DateTimeImmutable $updateAt = null;
 
     #[ORM\OneToOne(inversedBy: 'post', targetEntity: Thumbnail::class ,cascade:['persist', 'remove'])]
-    private Thumbnail $thumbnail;
+    private ?Thumbnail $thumbnail =null;
    
     public function __construct()
     {
