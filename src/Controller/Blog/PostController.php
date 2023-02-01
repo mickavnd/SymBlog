bbf7b6f1b579a11d3477a3be4fd2232209cc63b1
micka <mickavende.pro@gmail.com>
2023-02-02 00:03:21 +0100

    install paginator

--- src/Controller/Blog/PostController.php
+++ src/Controller/Blog/PostController.php
@@ -3,16 +3,24 @@
 namespace App\Controller\Blog;
 
 use App\Repository\PostRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class PostController extends AbstractController
 {
     #[Route('/', name: 'app_post' ,methods:['GET'])]
-    public function index(PostRepository $repository): Response
+    public function index(PostRepository $repository,PaginatorInterface $paginatorInterface,Request $request): Response
     {
-        $post = $repository->findPublished();
+        $data = $repository->findPublished();
+        $post = $paginatorInterface->paginate(
+            $data,
+            $request->query->getInt('page',1),
+            9
+
+        );
         
         return $this->render('pages/post/index.html.twig', [
             'post' => $post,
