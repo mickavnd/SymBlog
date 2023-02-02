c4bb189fc3b88df4dedcfeb9a32ce88fe76f4afe
micka <mickavende.pro@gmail.com>
2023-02-02 12:02:47 +0100

    update query  findpublished

--- src/Controller/Blog/PostController.php
+++ src/Controller/Blog/PostController.php
@@ -12,18 +12,12 @@ use Symfony\Component\HttpFoundation\Request;
 class PostController extends AbstractController
 {
     #[Route('/', name: 'app_post' ,methods:['GET'])]
-    public function index(PostRepository $repository,PaginatorInterface $paginatorInterface,Request $request): Response
+    public function index(PostRepository $repository,Request $request): Response
     {
-        $data = $repository->findPublished();
-        $post = $paginatorInterface->paginate(
-            $data,
-            $request->query->getInt('page',1),
-            9
-
-        );
         
         return $this->render('pages/post/index.html.twig', [
-            'post' => $post,
+            
+            'post' => $repository->findPublished($request->query->getInt('page',1)),
         ]);
     }
 }
--- src/Repository/PostRepository.php
+++ src/Repository/PostRepository.php
@@ -5,6 +5,8 @@ namespace App\Repository;
 use App\Entity\Post\Post;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Knp\Component\Pager\Pagination\PaginationInterface;
+use Knp\Component\Pager\PaginatorInterface;
 
 /**
  * @extends ServiceEntityRepository<Post>
@@ -16,7 +18,7 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class PostRepository extends ServiceEntityRepository
 {
-    public function __construct(ManagerRegistry $registry)
+    public function __construct(ManagerRegistry $registry, private PaginatorInterface $paginatorInterface)
     {
         parent::__construct($registry, Post::class);
     }
@@ -44,14 +46,17 @@ class PostRepository extends ServiceEntityRepository
      *
      * @return array
      */
-    public function findPublished():array
+    public function findPublished(int $page) :PaginationInterface
     {
-        return $this->createQueryBuilder("p")
+        $data =$this->createQueryBuilder("p")
             ->where('p.state LIKE :state')
             ->setParameter('state', '%STATE_PUBLISHED%')
             ->orderBy('p.createdAt','DESC')
             ->getQuery()
             ->getResult();
+
+            $post = $this->paginatorInterface->paginate($data,$page,9);
+        return $post;    
     
     }
 
